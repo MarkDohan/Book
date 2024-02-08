3e9a524d1b9e2ffa592a3cf7a12b3720940cf342
MarkDohan <122050255+MarkDohan@users.noreply.github.com>
2024-02-08 11:21:47 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function openNav(){
-    document.getElementById("myNav").style.width = "100%";
-    document.getElementsByClassName("wrapper").style.display = "fixed";
-    x.classList.toggle("change");
+    document.getElementById("myNav").style.width = "75%";  
 }
 
 function closeNav(){
     document.getElementById("myNav").style.width = "0%";
 }
+
+
+
+
